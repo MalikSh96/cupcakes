2dd674c639ca64269d01143cad25066c7b8787cb
Joklin <Joklin@10.50.137.190>
2018-03-01 12:42:12 +0100

    shoppingcart

---
+++ src/main/java/cupcake/Cupcake.java
@@ -0,0 +1,25 @@
+package cupcake;
+
+public class Cupcake {
+
+    private Cake_toppings topping;
+    private Cake_bottoms bottom;
+    private int bottomPrice, toppingPrice;
+
+    public Cupcake(Cake_toppings topping, Cake_bottoms bottom, int toppingPrice, int bottomPrice) {
+        this.topping = topping;
+        this.bottom = bottom;
+        this.bottomPrice = bottomPrice;
+        this.toppingPrice = toppingPrice;
+    }
+
+    public int getBottomPrice() {
+        return bottomPrice;
+    }
+
+    public int getToppingPrice() {
+        return toppingPrice;
+    }
+    
+    
+}
--- src/main/java/shoppingCart/ShoppingCart.java
+++ src/main/java/shoppingCart/ShoppingCart.java
@@ -1,19 +1,21 @@
 package shoppingCart;
 
-import LineItems.LineItems;
 import cupcake.Cupcake;
+import databaseConnect.DAO;
 import java.util.ArrayList;
 
 public class ShoppingCart {
 
-//    private int price;
-//    LineItems lineItem = new LineItems();
-//    ArrayList<Cupcake> lineItems = new ArrayList<>();
-//
-//    private ArrayList<String> shoppingCart(Cupcake cake) {
-//
-//        lineItems.add(line);
-//
-//        return lineItems;
-//    }
+    private int totalPrice;
+    ArrayList<Cupcake> shoppingCart = new ArrayList<>();
+    DAO dao;
+    Cupcake cake = new Cupcake(dao.getCakeTopping("derp"), dao.getCakeBottom("derp"), 
+            dao.getCakeTopping("derp").getTopping_price(), dao.getCakeBottom("derp").getBottom_price());
+    
+    private void shoppingCartAdd(Cupcake cake) {
+
+        shoppingCart.add(cake);
+        totalPrice += cake.getToppingPrice() + cake.getBottomPrice();
+        System.out.println(totalPrice);
+    }
 }
