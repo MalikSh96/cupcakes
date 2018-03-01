7d6ddb3e44c1440344f3de5474d0a217505a4e5c
Joklin <Joklin@10.50.137.190>
2018-03-01 11:56:03 +0100

    shoppingcart

--- src/main/java/LineItems/LineItems.java
+++ src/main/java/LineItems/LineItems.java
@@ -1,11 +1,15 @@
 package LineItems;
 
+import cupcake.Cake_bottoms;
+import cupcake.Cake_toppings;
+
 
 public class LineItems {
     
-    private String lineItems(String bottomCake, String topCake) {
+    private String lineItems(Cake_toppings topping, Cake_bottoms bottom) {
         
-        return bottomCake + ", " + topCake;       
+        
+        return "";
         
     }
     
